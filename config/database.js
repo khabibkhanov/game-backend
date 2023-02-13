ad82a271bb620d60a04fb919d642404f18119db6
khabibkhanov <hamidulloxon2002@gmail.com>
2023-02-13 09:34:21 +0500

    database added

--- config/database.js
+++ config/database.js
@@ -2,12 +2,13 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST'),
-      port: env.int('DATABASE_PORT'),
-      database: env('DATABASE_NAME'),
-      user: env('DATABASE_USERNAME'),
-      password: env('DATABASE_PASSWORD'),
-      ssl: env.bool('DATABASE_SSL', false),
+      url: env('DATABASE_URL'),
+      host: env('PGHOST'),
+      port: env.int('PGPORT'),
+      database: env('PGDATABASE'),
+      user: env('PGUSER'),
+      password: env('PGPASSWORD'),
+      ssl: env.bool('DATABASE_SSL_SELF', false),
     },
   },
 });
